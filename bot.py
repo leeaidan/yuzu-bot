8086f21b238eeb5a150d487d186957e54e929cea
Aidan Lee <meow123al@gmail.com>
2020-08-01 13:46:22 -0700

    Inital bot commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='.')
+
+
+@client.event
+async def on_ready():
+    print("Bot is ready.")
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+
+client.run('')
